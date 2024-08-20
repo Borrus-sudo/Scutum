e6c5e977bfa278cce816fa540174b0f83005dad1
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2024-08-20 09:51:44 +0530

    chore: update more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import * as vscode from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  vscode.window.showInformationMessage('Hello')
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    console.log(JSON.stringify(e, null, 2))
+  })
+
 })
 
 export { activate, deactivate }
